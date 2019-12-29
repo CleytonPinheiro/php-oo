081aa7057f7763488ddcf2a541b2f5b41acb0a2b
CleytonPinheiro <cleyton_pin@hotmail.com>
2019-12-29 13:03:18 -0300

    Trabalhando com heranças, modificadores de acesso e classes.

--- banco.php
+++ banco.php
@@ -1,10 +1,14 @@
 <?php
 
 require_once 'src/Conta.php';
+require_once 'src/Endereco.php';
 require_once 'src/Titular.php';
 require_once 'src/CPF.php';
 
-$vinicius = new Titular(new CPF('123.456.789-10'), 'Vinicius Dias');
+
+$endereco = new Endereco('Petropolis','um bairro', 'minha rua', '71B' );
+
+$vinicius = new Titular(new CPF('123.456.789-10'), 'Vinicius Dias', $endereco);
 $primeiraConta = new Conta($vinicius);
 $primeiraConta->deposita(500);
 $primeiraConta->saca(300); // isso é ok
@@ -13,10 +17,12 @@ echo $primeiraConta->recuperaNomeTitular() . PHP_EOL;
 echo $primeiraConta->recuperaCpfTitular() . PHP_EOL;
 echo $primeiraConta->recuperaSaldo() . PHP_EOL;
 
-$patricia = new Titular(new CPF('698.549.548-10'), 'Patricia');
+$patricia = new Titular(new CPF('698.549.548-10'), 'Patricia',$endereco);
 $segundaConta = new Conta($patricia);
 var_dump($segundaConta);
 
-$outra = new Conta(new Titular(new CPF('123.654.789-01'), 'Abcdefg'));
+$outroendereco = new Endereco('A','Bairro B', 'rua c', 'n 1D');
+
+$outra = new Conta(new Titular(new CPF('123.654.789-01'), 'Abcdefg',$outroendereco));
 unset($segundaConta);
 echo Conta::recuperaNumeroDeContas();
